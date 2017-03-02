7778d8fbd9ddd36ec01854d8275e5328ac145406
Alex Corvi <alex@arrayy.com>
2017-03-02 17:34:54 +0300

    refractored to ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import entities from "./list";
+
+export default function(input:string):string{
+	const matches = input.match(/&[a-z0-9#]+;/gi)||[];
+	for (var index = 0; index < matches.length; index++) {
+		var match = matches[index];
+		var token = match.substr(1,match.length-2).toLowerCase();
+		var replacement = entities[token];
+		if(replacement) {
+			input = input.split(match).join(replacement);
+		}
+	}
+	return input;
+}
